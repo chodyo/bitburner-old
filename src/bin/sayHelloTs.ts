19b7ee798821e4db6f4333eda2d8203aa1094c5a
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-01-15 12:56:31 -0500

    Add caller name to Logger, log->trace

--- src/bin/sayHelloTs.ts
+++ src/bin/sayHelloTs.ts
@@ -5,7 +5,7 @@ import { Logger } from "/lib/Logger";
 export async function main(ns: NS) {
     const logger = new Logger(ns);
 
-    logger.log("Hello from TypeScript o/");
+    logger.trace("Hello from TypeScript o/");
     logger.info("\tEverything seems to be in order :D");
     logger.warn("\tJust showing some colors");
     logger.err("Fake error, no panik");
--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -1,30 +1,47 @@
 import { NS } from "Bitburner";
 
 class Logger {
-    static INFO_LITERAL = "INFO   >";
-    static WARN_LITERAL = "WARN   >";
-    static ERR_LITERAL = "ERROR  >";
-    static TRACE_LITERAL = "TRACE  >";
+    static TRACE_LITERAL = "TRACE";
+    static INFO_LITERAL = "INFO";
+    static WARN_LITERAL = "WARN";
+    static ERR_LITERAL = "ERROR";
     ns: NS;
 
     constructor(ns: NS) {
         this.ns = ns;
     }
 
+    trace(msg: string, ...args: string[]) {
+        this.log(msg, this.caller(), Logger.TRACE_LITERAL, args);
+    }
+
     info(msg: string, ...args: string[]) {
-        this.ns.tprintf(`${Logger.INFO_LITERAL} ${msg}`, ...args);
+        this.log(msg, this.caller(), Logger.INFO_LITERAL, args);
     }
 
     warn(msg: string, ...args: string[]) {
-        this.ns.tprintf(`${Logger.WARN_LITERAL} ${msg}`, ...args);
+        this.log(msg, this.caller(), Logger.WARN_LITERAL, args);
     }
 
     err(msg: string, ...args: string[]) {
-        this.ns.tprintf(`${Logger.ERR_LITERAL} ${msg}`, ...args);
+        this.log(msg, this.caller(), Logger.ERR_LITERAL, args);
     }
 
-    log(msg: string, ...args: string[]) {
-        this.ns.tprintf(`${Logger.TRACE_LITERAL} ${msg}`, ...args);
+    private caller() {
+        try {
+            throw new Error();
+        } catch (e) {
+            // matches this function, the caller and the parent
+            const allMatches = e.stack.match(/([\w\.]+)@|at ([\w\.]+) \(/g);
+            // match parent function name
+            const parentMatches = allMatches[2].match(/([\w\.]+)@|at ([\w\.]+) \(/);
+            // return only name
+            return parentMatches[1] || parentMatches[2];
+        }
+    }
+
+    private log(msg: string, caller: string, level: string, args: string[]) {
+        this.ns.tprintf(`${level.padEnd(6)} > ${caller.padEnd(20)} msg::${msg}`, ...args);
     }
 }
 

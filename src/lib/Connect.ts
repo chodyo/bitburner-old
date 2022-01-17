f1590a5c8a3b4a0a62614029b3fa0bce43b88bae
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-01-17 16:53:41 -0500

    stub recursive connect

---
+++ src/lib/Connect.ts
@@ -0,0 +1,40 @@
+import { NS } from "Bitburner";
+import { Logger } from "/lib/Logger";
+
+const checkedHosts = new Map<string, boolean>();
+
+export async function main(ns: NS) {
+    const logger = new Logger(ns, { stdout: true });
+
+    const hostname = ns.args[0].toString();
+    checkedHosts.clear();
+    const connectChain = recursivelyFindHost(ns, "home", hostname);
+
+    logger.info("host found, connect by", connectChain.join("->"));
+
+    // connectChain.forEach((hostname) => {
+    //     connect: This singularity function requires Source-File 4 to run.
+    //     A power up you obtain later in the game.
+    //     It will be very obvious when and how you can obtain it.
+    //     ns.connect(hostname);
+    // });
+}
+
+function recursivelyFindHost(ns: NS, hostname: string, searchHost: string) {
+    if (checkedHosts.get(hostname)) {
+        return [];
+    }
+    checkedHosts.set(hostname, true);
+
+    for (const connectedHost of ns.scan(hostname)) {
+        if (connectedHost === searchHost) {
+            return [hostname, connectedHost];
+        }
+
+        const results: string[] = recursivelyFindHost(ns, connectedHost, searchHost);
+        if (results.length > 0) {
+            return [hostname, ...results];
+        }
+    }
+    return [];
+}

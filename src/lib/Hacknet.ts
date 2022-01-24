5b877d216617f668e4d58f78cd4962f305ee1a44
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-01-24 00:09:03 -0500

    try and fail to figure out how to calculate hacknet money gain rate in upgrades

--- src/lib/Hacknet.ts
+++ src/lib/Hacknet.ts
@@ -10,10 +10,23 @@ enum upgrades {
 }
 
 export async function main(ns: NS) {
+    const logger = new Logger(ns, { stdout: true });
+
+    const n = ns.args[0] as number;
+    const node = ns.hacknet.getNodeStats(n);
+    const levelUpgrade = ns.hacknet.getLevelUpgradeCost(n, 1) === Infinity ? node.level : node.level + 1;
+    const ramUpgrade = ns.hacknet.getRamUpgradeCost(n, 1) === Infinity ? node.ram : node.ram * 2;
+    const coreUpgrade = ns.hacknet.getCoreUpgradeCost(n, 1) === Infinity ? node.cores : node.cores + 1;
+    logger.info(`
+        production=${ns.nFormat(node.production, $)}/sec
+        level=+${ns.nFormat(moneyGainRate(ns, { n: n, level: levelUpgrade }) - node.production, $)}/sec
+        ram=+${ns.nFormat(moneyGainRate(ns, { n: n, ram: ramUpgrade }) - node.production, $)}/sec
+        core=+${ns.nFormat(moneyGainRate(ns, { n: n, cores: coreUpgrade }) - node.production, $)}/sec`);
+    return;
+
     while (buyCheapestUpgrade(ns, desiredSavings(ns))) {
-        await ns.sleep(3000);
+        await ns.sleep(60000);
     }
-    const logger = new Logger(ns);
     logger.info("finished buying hacknet upgrades");
 }
 
@@ -67,7 +80,6 @@ function cheapestUpgrade(ns: NS) {
         cost: h.numNodes() < h.maxNumNodes() ? h.getPurchaseNodeCost() : Infinity,
         fn: upgrades.purchase,
         n: -1,
-        roi: 0, // todo
     };
 
     Array(h.numNodes())
@@ -88,4 +100,37 @@ function cheapestUpgrade(ns: NS) {
         });
 
     return c;
+}
+
+function moneyGainRate(
+    ns: NS,
+    opts: {
+        n?: number;
+        level?: number;
+        ram?: number;
+        cores?: number;
+    },
+    mult = ns.getHacknetMultipliers().production
+) {
+    if (!opts.n === undefined && (opts.level === undefined || opts.ram === undefined || opts.cores === undefined)) {
+        throw new Error(
+            `missing opts - either specify the node number n, or provide the raw values of the node's level, ram, and cores; n=${opts.n} level=${opts.level} ram=${opts.ram} cores=${opts.cores}`
+        );
+    }
+
+    opts.level = !opts.level ? ns.hacknet.getNodeStats(opts.n!).level : opts.level;
+    opts.ram = !opts.ram ? ns.hacknet.getNodeStats(opts.n!).ram : opts.ram;
+    opts.cores = !opts.cores ? ns.hacknet.getNodeStats(opts.n!).cores : opts.cores;
+
+    // HacknetNodeConstants.MoneyGainPerLevel
+    const gainPerLevel = 1.5;
+
+    const levelMult = opts.level! * gainPerLevel;
+    const ramMult = Math.pow(1.035, opts.ram! - 1);
+    const coresMult = (opts.cores! + 5) / 6;
+
+    // BitNodeMultipliers.HacknetNodeMoney - presumably this will be built in to the production multiplier?
+    const bitNodeMult = 1;
+
+    return levelMult * ramMult * coresMult * mult * bitNodeMult;
 }

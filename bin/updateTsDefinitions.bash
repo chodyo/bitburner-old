cf7bdfd6583e3f3a2eeda74490bdd0fe33a94e82
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-01-24 08:13:47 -0500

    Tweak type defs to fit single module

--- bin/updateTsDefinitions.bash
+++ bin/updateTsDefinitions.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Base download
 URL="https://raw.githubusercontent.com/danielyxie/bitburner/master/dist/bitburner.d.ts"
-wget -O "src/lib/Bitburner.t.ts" "${URL}"
+outFile="src/lib/Bitburner.t.ts"
+wget -O "${outFile}" "${URL}"
+
+# The game defines objects in Bitburner.t.ts in a slightly different format than we want them
+
+# Remove all the existing exports so we can package it up in our own module
+sed -i 's/export declare //' $outFile
+
+# Remove the empty export at the end of the file
+sed -i 's/export { }//' $outFile
+
+# Export everything in its own module
+sed -i '1s/^/declare module "Bitburner" {\n/' $outFile
+echo '}' >> $outFile

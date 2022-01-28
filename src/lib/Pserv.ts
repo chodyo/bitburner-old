b2f35a94ed7a7e92b9a8c6cfd517370d17462ee6
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-01-27 20:50:57 -0500

    fix pserv buyer - more flexible, easier to read

--- src/lib/Pserv.ts
+++ src/lib/Pserv.ts
@@ -6,75 +6,61 @@ import { $, desiredSavings } from "/lib/Money";
 import { GB } from "/lib/Mem";
 
 export async function main(ns: NS) {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        await buyPservUpgrade(ns);
-        await ns.asleep(60000);
-    }
-}
-
-export async function buyPservUpgrade(ns: NS) {
     const logger = new Logger(ns);
 
-    const limit = ns.getPurchasedServerLimit();
-    const myPservs = ns
-        .getPurchasedServers()
-        .map((pserv) => {
-            return {
-                hostname: pserv,
-                ram: ns.getServerMaxRam(pserv),
-            };
-        })
-        .sort((a, b) => a.ram - b.ram);
-
-    const dollarsPerGig = 55000; // i think? just based on the store pages
-    const spendableCash = ns.getServerMoneyAvailable("home") - desiredSavings(ns);
-    let ram = Math.floor(spendableCash / dollarsPerGig);
-    let bitPos = 0;
-    while (ram > 0) {
-        bitPos++;
-        ram >>= 1;
-    }
-    ram = Math.pow(2, bitPos - 1);
-    ram = Math.min(ram, ns.getPurchasedServerMaxRam());
-
-    const worthIt = ram >= 8;
-    const isAnUpgrade = myPservs[0]?.ram < ram || true;
-    if (!worthIt || !isAnUpgrade) {
-        logger.trace(
-            `not buying any pservs worthIt=${worthIt} isAnUpgrade=${isAnUpgrade} ram=${ns.nFormat(
-                ram * 1e9,
-                GB
-            )} minPservRam=${ns.nFormat(myPservs[0]?.ram * 1e9 || 0, GB)} spareCash=${ns.nFormat(spendableCash, $)}`
-        );
-        return;
-    }
-
-    logger.trace(
-        "could buy a pserv",
-        ns.nFormat(spendableCash, $),
-        ns.nFormat(ns.getPurchasedServerCost(ram), $),
-        ns.nFormat(ram * 1e9, GB)
-    );
-
-    if (myPservs.length === limit) {
-        const pservToSell = myPservs[0];
-        ns.killall(pservToSell.hostname);
-        while (ns.getServerUsedRam(pservToSell.hostname)) {
-            await ns.asleep(100);
+    while (pservUpgradable(ns)) {
+        let upgradeCount = 0;
+        while (await buyPservUpgrade(ns)) {
+            upgradeCount++;
+            await ns.sleep(10);
         }
-        ns.deleteServer(pservToSell.hostname);
-        logger.info(
-            "deleted pserv to make room for some chonkier ones",
-            pservToSell.hostname,
-            ns.nFormat(pservToSell.ram * 1e9, GB)
-        );
+        if (upgradeCount > 0) {
+            logger.toast(`finished buying ${upgradeCount} pserv upgrades :)`);
+        }
+        await ns.sleep(60000);
     }
+    logger.toast("exiting pserv buyer", "info");
+}
+
+function pservUpgradable(ns: NS) {
+    const pservs = ns.getPurchasedServers();
+    if (pservs.length < ns.getPurchasedServerLimit()) return true;
+
+    const pservMaxRam = ns.getPurchasedServerMaxRam();
+    return pservs.some((hostname) => {
+        return ns.getServerMaxRam(hostname) < pservMaxRam;
+    });
+}
+
+async function buyPservUpgrade(ns: NS) {
+    const logger = new Logger(ns);
+
+    const ram = maxAffordableRam(ns);
+    if (ram < 8) {
+        return false;
+    }
+
+    if (ns.getPurchasedServers().length === ns.getPurchasedServerLimit()) {
+        const worstPserv = worstOwnedPserv(ns);
+        const worstRam = ns.getServerMaxRam(worstPserv);
+        if (worstRam >= ram) return false;
+
+        const sold = await sellWorstPserv(ns, worstPserv);
+        if (sold) {
+            logger.info(
+                "deleted pserv to make room for some chonkier ones",
+                worstPserv,
+                ns.nFormat(worstRam * 1e9, GB)
+            );
+        }
+    }
+
+    logger.trace("could buy a pserv", ns.nFormat(ns.getPurchasedServerCost(ram), $), ns.nFormat(ram * 1e9, GB));
 
     const hostname = ns.purchaseServer("pserv", ram);
     if (!hostname) {
         logger.error("tried and failed to buy a pserv");
-        return;
+        return false;
     }
     logger.info("bought", hostname, ns.nFormat(ram * 1e9, GB));
 
@@ -88,4 +74,41 @@ export async function buyPservUpgrade(ns: NS) {
         params.moneyThreshold.toString(),
         params.securityThreshold.toString()
     );
+    return true;
+}
+
+function maxAffordableRam(ns: NS) {
+    const cash = ns.getServerMoneyAvailable("home") - desiredSavings(ns);
+    let ram = ns.getPurchasedServerMaxRam();
+    while (ram > 0 && ns.getPurchasedServerCost(ram) > cash) {
+        ram >>= 1;
+    }
+    return ram;
+}
+
+function worstOwnedPserv(ns: NS) {
+    const pservs = ns.getPurchasedServers();
+    if (pservs.length === 0) {
+        return "";
+    }
+
+    return pservs
+        .map((pserv) => {
+            return {
+                hostname: pserv,
+                ram: ns.getServerMaxRam(pserv),
+            };
+        })
+        .sort((a, b) => a.ram - b.ram)[0].hostname;
+}
+
+async function sellWorstPserv(ns: NS, hostname: string) {
+    if (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) return true;
+
+    ns.killall(hostname);
+    while (ns.getServerUsedRam(hostname)) {
+        await ns.asleep(10);
+    }
+
+    return ns.deleteServer(hostname);
 }

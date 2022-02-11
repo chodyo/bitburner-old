2426ed8068e19821f4c0713f725f49e4dcf4ae8c
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-02-11 08:44:01 -0500

    try spreading out scripts to big enough machines

--- src/bin/optimize.ts
+++ src/bin/optimize.ts
@@ -1,33 +1,66 @@
 import { NS } from "Bitburner";
 import { Logger } from "/lib/Logger";
-import { alreadyDeployed } from "/lib/Deploy";
+import { gainRootAccess } from "/lib/Root";
 
 export async function main(ns: NS) {
-    // Game keeps crashing on reset, maybe delaying the start will help
-    await ns.sleep(1 * 60 * 1000);
-
     const logger = new Logger(ns);
     logger.trace("starting");
 
+    const deploys = [
+        {
+            scriptname: "/lib/Hacknet.js",
+            destination: "foodnstuff",
+            dependencies: ["/lib/Logger.js"],
+            args: [],
+        },
+        {
+            scriptname: "/lib/CodingContracts.js",
+            destination: "iron-gym",
+            dependencies: [],
+            args: ["autosolve", "--quiet", "--daemon"],
+        },
+        {
+            scriptname: "/lib/Home.js",
+            destination: "sigma-cosmetics",
+            dependencies: ["/lib/Logger.js", "/lib/Money.js"],
+            args: [],
+        },
+        {
+            scriptname: "/lib/Darkweb.js",
+            destination: "joesguns",
+            dependencies: ["/lib/Logger.js", "/lib/Money.js"],
+            args: [],
+        },
+    ];
+    for (const deploy of deploys) {
+        if (!gainRootAccess(ns, deploy.destination)) {
+            logger.toast(`no root access on ${deploy.destination} for ${deploy.scriptname}`, "error");
+            continue;
+        }
+
+        if (deploy.dependencies.length > 0) await ns.scp(deploy.dependencies, deploy.destination);
+        await ns.scp(deploy.scriptname, deploy.destination);
+
+        ns.scriptKill(deploy.scriptname, deploy.destination);
+        const result: "success" | "error" = ns.exec(deploy.scriptname, deploy.destination, 1, ...deploy.args)
+            ? "success"
+            : "error";
+        logger.toast(`running script ${deploy.scriptname} on ${deploy.destination}: ${result}`, result);
+    }
+
     // scripts that know when there's nothing left to do and exit on their own
-    const oneTimeScripts = ["/lib/Darkweb.js", "/lib/Pserv.js"];
+    const oneTimeScripts = ["/lib/Pserv.js", "/lib/Faction.js", "/bin/startHack.js"];
     oneTimeScripts.forEach((filename) => {
-        if (alreadyDeployed(ns, filename, "home")) return;
-        const result: "success" | "error" = ns.run(filename) ? "success" : "error";
+        ns.scriptKill(filename, "home");
+        const result: "success" | "error" = ns.exec(filename, "home") ? "success" : "error";
         logger.toast(`running script ${filename} on home: ${result}`, result);
     });
 
-    const backgroundScripts = [
-        "/lib/Home.js",
-        "/lib/Hacknet.js",
-        "/lib/Faction.js",
-        "/bin/startHack.js",
-        "/bin/contracts.js",
-    ];
-    while (true) {
+    const backgroundScripts = [];
+    while (backgroundScripts.length > 0) {
         backgroundScripts.forEach((filename) => {
-            if (alreadyDeployed(ns, filename, "home")) return;
-            const result: "success" | "error" = ns.run(filename) ? "success" : "error";
+            ns.scriptKill(filename, "home");
+            const result: "success" | "error" = ns.exec(filename, "home") ? "success" : "error";
             logger.toast(`running script ${filename} on home: ${result}`, result);
         });
         await ns.asleep(60000);

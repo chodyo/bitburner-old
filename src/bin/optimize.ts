4d7c239d64a35561cb6e4baef3710f871eaf9c05
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2022-02-25 12:50:36 -0500

    run some scripts fast, some slow

--- src/bin/optimize.ts
+++ src/bin/optimize.ts
@@ -10,10 +10,8 @@ export async function main(ns: NS) {
     logger.trace("starting");
     logger.toast("reminder that hacknet has the bitnode mult hardcoded to 0.05", "warning");
 
-    const controlPortHandle = ns.getPortHandle(PortNumbers.Control);
-
     const scripts = [
-        { name: "/bin/startHack.js", active: true },
+        { name: "/bin/startHack.js", active: true, runFast: true },
         { name: "/bin/darkweb.js", active: true },
         { name: "/bin/hacknet.js", active: true },
         { name: "/bin/contracts.js", active: true },
@@ -23,44 +21,73 @@ export async function main(ns: NS) {
         { name: "/bin/redpill.js", active: true },
     ];
 
+    // i'm going to run the startHack script this many ms apart, 1000 times. so the secondary
+    // scripts will run once per this many seconds. the math will need to change if i add another
+    // runfast script
+    const secondaryScriptRunFrequencySeconds = 20;
+
     while (scripts.some((script) => script.active)) {
-        const control = new Map<string, ScriptResult>();
-        while (!controlPortHandle.empty()) {
-            const msg: ScriptResult = JSON.parse(controlPortHandle.read().toString());
-            control.set(msg.script, msg);
+        updateScriptsBasedOnCtrlMsgs(ns, scripts);
+
+        const activeScripts = scripts.filter((script) => script.active);
+
+        const runFastScripts = activeScripts.filter((script) => script.runFast);
+        for (let i = 0; i < 1000; i++) {
+            for (const fastScript of runFastScripts) await runScript(ns, fastScript);
+            await ns.sleep(secondaryScriptRunFrequencySeconds);
         }
 
-        for (let i in scripts) {
-            const script = scripts[i];
+        const secondaryScripts = activeScripts.filter((script) => !script.runFast);
+        for (const secondaryScript of secondaryScripts) await runScript(ns, secondaryScript);
+    }
+}
 
-            // 2.00GB | stanek.get (fn)
-            // const controlMsg = control.get(script.name);
-            const controlMsg = control["get"](script.name);
-            if (controlMsg) {
-                logger.trace("control msg", controlMsg);
-                if (controlMsg.next === "exit") scripts[i].active = false;
-                else scripts[i].active = true;
+async function runScript(ns: NS, script: { name: string; args?: string[] }) {
+    const logger = new Logger(ns);
 
-                if (controlMsg.done === "saveMoney") {
-                    scripts
-                        .filter((script) => scriptsThatCostMeMoney.includes(script.name))
-                        .forEach((script) => {
-                            script.active = false;
-                        });
-                }
+    const pid = ns.run(script.name, 1, ...(script.args ? script.args : []));
+    if (!pid) {
+        logger.toast(`optimize failed to run ${script.name} - recommend temporarily disabling it`);
+        return;
+    }
 
-                if (controlMsg.next && script.args && script.args.length > 0)
-                    scripts[i].args = [script.args[0], controlMsg.next];
+    const waitTimeMs = 10;
+    while (ns.getRunningScript(pid)) await ns.sleep(waitTimeMs);
+}
+
+function updateScriptsBasedOnCtrlMsgs(ns: NS, scripts: { name: string; active: boolean; args?: string[] }[]) {
+    const logger = new Logger(ns);
+
+    const controlPortHandle = ns.getPortHandle(PortNumbers.Control);
+    const control = new Map<string, ScriptResult>();
+
+    while (!controlPortHandle.empty()) {
+        const msg: ScriptResult = JSON.parse(controlPortHandle.read().toString());
+        control.set(msg.script, msg);
+    }
+
+    for (let i in scripts) {
+        const script = scripts[i];
+
+        // 2.00GB | stanek.get (fn)
+        // const controlMsg = control.get(script.name);
+        const controlMsg = control["get"](script.name);
+        if (controlMsg) {
+            control.delete(script.name);
+            logger.trace("control msg", controlMsg);
+            if (controlMsg.next === "exit") scripts[i].active = false;
+            else scripts[i].active = true;
+
+            if (controlMsg.done === "saveMoney") {
+                scripts
+                    .filter((script) => scriptsThatCostMeMoney.includes(script.name))
+                    .forEach((script) => {
+                        script.active = false;
+                    });
             }
 
-            if (!script.active) continue;
-
-            const pid = ns.run(script.name, 1, ...(script.args ? script.args : []));
-            if (!pid) {
-                logger.toast(`optimize failed to run ${script.name} - recommend temporarily disabling it`);
-                continue;
-            }
-            while (ns.getRunningScript(pid)) await ns.sleep(2 * 1000);
+            if (controlMsg.next && script.args && script.args.length > 0)
+                scripts[i].args = [script.args[0], controlMsg.next];
         }
     }
 }
